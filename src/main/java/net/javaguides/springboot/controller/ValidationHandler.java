7a2bfd447ddc1d397cbc49af63f2da4f69d2281e
Ramesh Fadatare <ramesh24fadatare@gmail.com>
2023-03-28 08:34:32 +0530

    Upgraded to spring boot 3

--- src/main/java/net/javaguides/springboot/controller/ValidationHandler.java
+++ src/main/java/net/javaguides/springboot/controller/ValidationHandler.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,7 +17,7 @@ public class ValidationHandler extends ResponseEntityExceptionHandler{
 	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 		
 		Map<String, String> errors = new HashMap<>();
 		ex.getBindingResult().getAllErrors().forEach((error) ->{
